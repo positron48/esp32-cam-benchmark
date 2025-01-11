ba026a07fd05f4daa383b684eae9aac6b15ad760
Filatov Anton <filatov@intaro.ru>
2025-01-11 13:06:59 +0300

    снова рабочий стрим

--- src/video_http.h
+++ src/video_http.h
@@ -14,50 +14,55 @@
 // Global web server instance
 extern AsyncWebServer server;
 
+class AsyncMjpegStreamHandler: public AsyncWebHandler {
+public:
+    AsyncMjpegStreamHandler() {}
+    virtual ~AsyncMjpegStreamHandler() {}
+
+    bool canHandle(AsyncWebServerRequest *request) {
+        return request->url() == "/video" && request->method() == HTTP_GET;
+    }
+
+    void handleRequest(AsyncWebServerRequest *request) {
+        AsyncResponseStream *response = request->beginResponseStream(STREAM_CONTENT_TYPE);
+        response->addHeader("Access-Control-Allow-Origin", "*");
+        
+        camera_fb_t *fb = esp_camera_fb_get();
+        if (!fb) {
+            request->send(500);
+            return;
+        }
+
+        response->write(STREAM_BOUNDARY, strlen(STREAM_BOUNDARY));
+        
+        char buf[64];
+        size_t len = snprintf(buf, sizeof(buf), STREAM_PART, fb->len);
+        response->write(buf, len);
+        response->write((char *)fb->buf, fb->len);
+        
+        esp_camera_fb_return(fb);
+        request->send(response);
+    }
+};
+
 // Initialize HTTP video streaming
 void initVideoHTTP() {
     server.on("/stream", HTTP_GET, [](AsyncWebServerRequest* request) {
         AsyncWebServerResponse* response = request->beginResponse(
             200, 
             "text/html",
-            "<html><body><img src='/video' style='width:100%;height:auto;'/></body></html>"
+            "<html><head>"
+            "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+            "<style>img { width: 100%; height: auto; }</style>"
+            "</head><body>"
+            "<img src='/video' />"  // /video endpoint returns MJPEG stream which browser will automatically update
+            "</body></html>"
         );
         response->addHeader("Access-Control-Allow-Origin", "*");
         request->send(response);
     });
 
-    server.on(
-        "/video",
-        HTTP_GET,
-        [](AsyncWebServerRequest* request) {
-            static char* part_buf = (char*)malloc(64);
-            static char* boundary = (char*)malloc(strlen(STREAM_BOUNDARY) + 1);
-            if (!part_buf || !boundary) {
-                request->send(500);
-                return;
-            }
-            
-            camera_fb_t* fb = esp_camera_fb_get();
-            if (!fb) {
-                request->send(500);
-                return;
-            }
-
-            AsyncResponseStream* response = request->beginResponseStream(STREAM_CONTENT_TYPE);
-            response->addHeader("Access-Control-Allow-Origin", "*");
-            
-            strcpy(boundary, STREAM_BOUNDARY);
-            response->write(boundary, strlen(boundary));
-            
-            sprintf(part_buf, STREAM_PART, fb->len);
-            response->write(part_buf, strlen(part_buf));
-            response->write((char*)fb->buf, fb->len);
-            
-            esp_camera_fb_return(fb);
-            request->send(response);
-        },
-        NULL
-    );
+    server.addHandler(new AsyncMjpegStreamHandler());
 }
 
 // Handle HTTP video streaming - now just maintains frame rate
